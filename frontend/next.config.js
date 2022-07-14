eaa0ad4366c30b2b76bf84dc75e66d688ad9a935
Cheon SeungJun <sjc0829@naver.com>
2022-07-14 19:44:08 +0900

    [MOD] react 프로젝트 초기 세팅 완료

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,18 @@
+const { truncateSync } = require('fs');
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack5 : true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs : false, net : false, tls : false, cardinal : false };
+    return config;
+  },
   swcMinify: true,
+  sassOptions : {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
 import Header from '../../components/header';
 
 export default function Home () {
-    return <Header />;
+
+    function test() {
+      fetch('http://localhost:3000/api/test')
+      .then(res => res.json)
+      .then(data => console.log(data));
+    }
+
+    return <section><Header />
+    <button onClick={test}>
+      test
+    </button>
+
+    </section>;
   }
